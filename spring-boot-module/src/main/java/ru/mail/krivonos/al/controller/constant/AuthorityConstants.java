5dc73c87b568ffebe4d69ee07120078a0720d9b1
krivonos-alex <krivonos-al@mail.ru>
2019-05-12 21:50:45 +0300

    Added Spring Security configuration with all need constants and exceptions.

---
+++ spring-boot-module/src/main/java/ru/mail/krivonos/al/controller/constant/AuthorityConstants.java
@@ -0,0 +1,9 @@
+package ru.mail.krivonos.al.controller.constant;
+
+public class AuthorityConstants {
+
+    public static final String ADMIN_AUTHORITY_NAME = "Administrator";
+
+    private AuthorityConstants() {
+    }
+}
---
+++ spring-boot-module/src/main/java/ru/mail/krivonos/al/controller/constant/URLParametersConstants.java
@@ -0,0 +1,15 @@
+package ru.mail.krivonos.al.controller.constant;
+
+public class URLParametersConstants {
+
+    public static final String ADDED_PARAM = "added";
+    public static final String UPDATED_POSITIVE_PARAM = "updated";
+    public static final String UPDATED_NEGATIVE_PARAM = "updated_zero";
+    public static final String DELETED_POSITIVE_PARAM = "deleted";
+    public static final String DELETED_NEGATIVE_PARAM = "deleted_zero";
+    public static final String PASSWORD_CHANGE_POSITIVE_PARAM = "password_changed";
+    public static final String PASSWORD_CHANGE_NEGATIVE_PARAM = "password_error";
+
+    private URLParametersConstants() {
+    }
+}
