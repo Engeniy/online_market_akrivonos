ea7586838720ac08afe65ce52737918c7c9bd5d2
krivonos-alex <krivonos-al@mail.ru>
2019-05-16 10:04:16 +0300

    Removed unused constants

--- spring-boot-module/src/main/java/ru/mail/krivonos/al/controller/constant/URLConstants.java
+++ spring-boot-module/src/main/java/ru/mail/krivonos/al/controller/constant/URLConstants.java
@@ -8,13 +8,11 @@ public class URLConstants {
     public static final String FORBIDDEN_PAGE_URL = "/403";
     public static final String INTERNAL_ERROR_PAGE_URL = "/500";
     public static final String USERS_PAGE_URL = "/private/users";
-    public static final String USERS_PAGE_WITH_PAGE_NUMBER_URL = "/private/users/{page}";
     public static final String USERS_ADD_PAGE_URL = "/private/users/add";
     public static final String USERS_UPDATE_URL = "/private/users/{id}/update";
     public static final String USERS_DELETE_URL = "/private/users/delete";
     public static final String USERS_PASSWORD_CHANGE_URL = "/private/users/{id}/password";
     public static final String REVIEWS_PAGE_URL = "/reviews";
-    public static final String REVIEWS_PAGE_WITH_PAGE_NUMBER_URL = "/reviews/{page}";
     public static final String REVIEWS_DELETE_URL = "/reviews/{id}/delete";
     public static final String REVIEWS_UPDATE_URL = "/reviews/update";
     public static final String BOOTSTRAP_CONTENT_URL = "/webjars/**";
