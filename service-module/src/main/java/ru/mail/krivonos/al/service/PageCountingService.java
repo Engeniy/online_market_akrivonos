46b95cd0787f7c98b481a49da493279b6ed7676d
krivonos-alex <krivonos-al@mail.ru>
2019-05-23 14:36:58 +0300

    Edited constants, renamed method in PageCountingService.

--- service-module/src/main/java/ru/mail/krivonos/al/service/PageCountingService.java
+++ service-module/src/main/java/ru/mail/krivonos/al/service/PageCountingService.java
@@ -2,7 +2,7 @@ package ru.mail.krivonos.al.service;
 
 public interface PageCountingService {
 
-    int countPages(int itemsNumber, int pageLimit);
+    int getCountOfPages(int itemsNumber, int pageLimit);
 
     int getOffset(int pageNumber, int limit);
 
