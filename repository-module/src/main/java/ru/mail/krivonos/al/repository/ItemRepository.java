af34277b0d054363d343a36be35200370006d38c
krivonos-alex <krivonos-al@mail.ru>
2019-05-25 14:30:08 +0300

    Added method for search by unique number into ItemRepository.

--- repository-module/src/main/java/ru/mail/krivonos/al/repository/ItemRepository.java
+++ repository-module/src/main/java/ru/mail/krivonos/al/repository/ItemRepository.java
@@ -3,4 +3,6 @@ package ru.mail.krivonos.al.repository;
 import ru.mail.krivonos.al.repository.model.Item;
 
 public interface ItemRepository extends GenericRepository<Long, Item> {
+
+    Item findItemByUniqueNumber(String uniqueNumber);
 }
--- repository-module/src/main/java/ru/mail/krivonos/al/repository/impl/ItemRepositoryImpl.java
+++ repository-module/src/main/java/ru/mail/krivonos/al/repository/impl/ItemRepositoryImpl.java
@@ -4,6 +4,21 @@ import org.springframework.stereotype.Repository;
 import ru.mail.krivonos.al.repository.ItemRepository;
 import ru.mail.krivonos.al.repository.model.Item;
 
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 @Repository("itemRepository")
 public class ItemRepositoryImpl extends GenericRepositoryImpl<Long, Item> implements ItemRepository {
+
+    @Override
+    public Item findItemByUniqueNumber(String uniqueNumber) {
+        String queryString = String.format("from %s %s", entityClass.getName(),
+                " where unique_number = :unique_number");
+        Query query = entityManager.createQuery(queryString).setParameter("unique_number", uniqueNumber);
+        try {
+            return (Item) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
