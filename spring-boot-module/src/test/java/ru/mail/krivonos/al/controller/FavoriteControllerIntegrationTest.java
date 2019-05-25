02cc3f81b921cea52758834def59eead36650dbe
krivonos-alex <krivonos-al@mail.ru>
2019-05-25 17:07:46 +0300

    Added additional test for UserController. Also added profile into db-changelog for default Customer User.

--- spring-boot-module/src/test/java/ru/mail/krivonos/al/controller/FavoriteControllerIntegrationTest.java
+++ spring-boot-module/src/test/java/ru/mail/krivonos/al/controller/FavoriteControllerIntegrationTest.java
@@ -12,16 +12,12 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.mail.krivonos.al.service.model.CommentDTO;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.mail.krivonos.al.controller.constant.URLConstants.ARTICLE_ADD_COMMENT_URL;
-import static ru.mail.krivonos.al.controller.constant.URLConstants.ARTICLE_DELETE_COMMENT_URL;
-import static ru.mail.krivonos.al.controller.constant.URLConstants.ARTICLE_PAGE_URL;
 import static ru.mail.krivonos.al.controller.constant.URLConstants.FAVORITE_ARTICLES_URL;
 
 @RunWith(SpringRunner.class)
